d523e54f71ae1eed3b0402854538c372d637c2be
matovu-farid <matovu90@gmail.com>
2023-11-20 06:22:21 +0300

    Made algorithm runner more extensible

--- lua/commands/algorithm.lua
+++ lua/commands/algorithm.lua
@@ -21,32 +21,12 @@ local function on_stdout(_, data, _)
 end
 
 
-
-
-
-
-
 local function on_exit(output)
   return function()
-    local output_file = vim.fn.expand("%:p:h") .. "/output.txt"
     vim.api.nvim_buf_set_lines(output, 0, -1, false, output_lines)
-    local file = io.open(output_file, "w")
-    if file then
-      file:write(table.concat(output_lines, "\n"))
-      file:flush()
-      file:close()
-
-      -- Update the buffer if it's open
-      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-        if vim.api.nvim_buf_get_name(buf) == output_file then
-          vim.api.nvim_buf_set_lines(buf, 0, -1, false, output_lines)
-          vim.api.nvim_buf_set_option(buf, 'modified', false)
-        end
-      end
-    end
   end
 end
-local function clone_input(input_bfr)
+local function cache_input(input_bfr)
   local input_file = vim.fn.expand("%:p:h") .. "/.input"
   local file = io.open(input_file, "w")
   if file then
@@ -56,22 +36,22 @@ local function clone_input(input_bfr)
     file:close()
   end
 end
+local function clone_input(input_bfr)
+  local input_file = vim.fn.expand("%:p:h") .. "/.input"
+  local file = io.open(input_file, "r")
+  if file then
+    local lines = {}
+    for line in file:lines() do
+      table.insert(lines, line)
+    end
+    vim.api.nvim_buf_set_lines(input_bfr, 0, -1, false, lines)
+    file:close()
+  end
+end
 
--- bufs - {input, output}
 
 
 
-
--- timeout jobs at 5s with gtimeout
-
-local jobs = {
-  { "crun", "main.cpp" },
-  { "prun", "main.py" },
-}
-local projectNames = {
-  "algorithms",
-
-}
 local function show_split(split, main_buf)
   split:mount()
 
@@ -82,27 +62,6 @@ local function show_split(split, main_buf)
     end
   })
 end
-
-local function create_splits(main)
-        local input = Split({
-        relative = "editor",
-        position = "right",
-        size = "20%",
-        enter = false
-      })
-      show_split(input, main)
-      local output = Split({
-        relative = {
-          type = "win",
-          winid = input.winid,
-        },
-        position = "bottom",
-        size = "50%",
-        enter = false
-      })
-      show_split(output, main)
-  return { input = input.bufnr, output = output.bufnr, main = main }
-end
 local function close_other_buffers(main)
   local bufs = vim.api.nvim_list_bufs()
   for _, buf in ipairs(bufs) do
@@ -111,23 +70,38 @@ local function close_other_buffers(main)
       vim.api.nvim_buf_delete(buf, { force = true })
     end
   end
-  create_splits(main)
 end
+local function create_splits(main)
+  close_other_buffers(main)
+  local input = Split({
+    relative = "editor",
+    position = "right",
+    size = "20%",
+    enter = false
+  })
+  show_split(input, main)
+  local output = Split({
+    relative = {
+      type = "win",
+      winid = input.winid,
+    },
+    position = "bottom",
+    size = "50%",
+    enter = false
+  })
+  show_split(output, main)
+  return { input = input.bufnr, output = output.bufnr, main = main }
+end
+
 local function run_main(job, bufs)
   vim.api.nvim_create_autocmd({ "BufLeave" }, {
     buffer = bufs.input,
     callback = function()
-      clone_input(bufs.input)
+      cache_input(bufs.input)
     end
   })
-  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-    buffer = bufs.main,
-    callback = function()
-      close_other_buffers(bufs.main)
-    end
+  clone_input(bufs.input)
 
-  })
-  -- Start the job
   return function()
     job_id = vim.fn.jobstart(job, {
       on_stdout = on_stdout,
@@ -137,34 +111,33 @@ local function run_main(job, bufs)
     output_lines = {}
   end
 end
-local function create_job_command(job)
-  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-    pattern = { job[2] },
-    callback = function()
-      local projectName = vim.fn.expand("%:p:h:t")
 
-      local isRegisteredProject = false
-      for _, name in ipairs(projectNames) do
-        if name == projectName then
-          isRegisteredProject = true
-        end
-      end
-      if not isRegisteredProject then
-        return
-      end
-      local main_file = vim.api.nvim_get_current_buf()
-
-
-      vim.api.nvim_command("Copilot disable")
-
-
-      vim.keymap.set("n", "<leader>x", run_main(job, create_splits(main_file)),
-        { silent = true, desc = "Run main.cpp" })
-    end,
-    group = group
-  })
+local function create_runner(job)
+  return function()
+    local main_file = vim.api.nvim_get_current_buf()
+    vim.api.nvim_command("Copilot disable")
+    vim.keymap.set("n", "<leader>x", run_main(job, create_splits(main_file)),
+      { silent = true, desc = "Run file" })
+  end
 end
+local function init_algo()
+  local file_extension = vim.fn.expand("%:e")
+  local jobs = {
+    cpp = { "crun", "main.cpp" },
+    py = { "prun", "main.py" },
+  }
+  local job = jobs[file_extension]
 
-for _, job in ipairs(jobs) do
-  create_job_command(job)
+  if job then
+    vim.keymap.set("n", "<leader>x",
+      create_runner(job)
+      , { silent = true, desc = "Run" })
+  end
 end
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = "*",
+  callback = init_algo,
+})
+
+
+
